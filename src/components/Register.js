6a64bf0dd63424eb1a989cdb0b76da2e7058220f
fajrinnurutami <fajrin145@gmail.com>
2021-04-30 09:50:48 +0700

    adding navbar, header, page contact us

---
+++ src/components/Register.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Register = () =>{
+    return (
+        <h1>Ini adalah halaman register</h1>
+    );
+}
+
+export default Register;
